51a495689c17c7a836750490981619bfee629df3
lateral-neck-stretch <zhang.anthony97@gmail.com>
2024-11-13 15:46:10 -0500

    cracklePop

--- cracklePop.js
+++ cracklePop.js
@@ -6,3 +6,25 @@
  * If it's divisible by 5, print Pop. If it's divisible by both 3 and 5,
  * print CracklePop. You can use any language.
  */
+
+function cracklePop(number = 0) {
+  if (number > 100) {
+    return;
+  }
+
+  const divThree = number % 3 === 0;
+  const divFive = number % 5 === 0;
+
+  if (divThree && divFive) {
+    console.log("CracklePop");
+  } else if (divThree) {
+    console.log("Crackle");
+  } else if (divFive) {
+    console.log("Pop");
+  } else {
+    console.log(number);
+  }
+  return cracklePop(number + 1);
+}
+
+cracklePop();
